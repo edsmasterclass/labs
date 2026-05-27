e09c679c94699220b3cb75412a66f39d33df4675
U237659 <prathamesh.jagtap@suncorp.com.au>
2026-05-27 10:39:53 +1000

    feat: add eyebrow enhancement, list and view-switcher variations to cards block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,17 +1,39 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
+
+    // Detect italic text in body and extract as eyebrow label
+    const body = li.querySelector('.cards-card-body');
+    const em = body?.querySelector('em');
+    if (em) {
+      const eyebrow = document.createElement('div');
+      eyebrow.className = 'cards-card-eyebrow';
+      eyebrow.textContent = em.textContent;
+      const p = em.closest('p');
+      if (p) p.remove();
+      else em.remove();
+      body.prepend(eyebrow);
+    }
+
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    img.closest('picture').replaceWith(
+      createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])
+    );
+  });
   block.replaceChildren(ul);
 }
