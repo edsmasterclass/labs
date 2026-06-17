d30e91d6a1bd7975bf7bd7a2fa7b52ee729e2fb2
MatijaSabo <matijasaboli37@gmail.com>
2026-06-17 10:20:28 +0100

    Exercise 2 - solution

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -13,5 +13,11 @@ export default function decorate(block) {
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('em').forEach((em) => {
+    const span = document.createElement('span');
+    span.className = 'cards-highlight';
+    span.append(...em.childNodes);
+    em.replaceWith(span);
+  });
   block.replaceChildren(ul);
 }
