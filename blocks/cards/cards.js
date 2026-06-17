1dd134f0a81b62b8b1c4668253e3f21059ebf229
nikstyagi <niktyagi@deloitte.co.uk>
2026-06-17 11:20:26 +0100

    added the switcher variation

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -31,9 +31,40 @@ export default function decorate(block) {
   });
 
   ul.querySelectorAll('picture > img').forEach((img) => {
-    img.closest('picture').replaceWith(
-      createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
-    );
+    img
+      .closest('picture')
+      .replaceWith(
+        createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
+      );
   });
   block.replaceChildren(ul);
+  if (block.classList.contains('view-switcher')) {
+    const toolbar = document.createElement('div');
+    toolbar.className = 'cards-toolbar';
+
+    const isList = block.classList.contains('list');
+
+    const gridBtn = document.createElement('button');
+    gridBtn.textContent = 'Grid';
+    gridBtn.className = `cards-toolbar-btn${isList ? '' : ' active'}`;
+
+    const listBtn = document.createElement('button');
+    listBtn.textContent = 'List';
+    listBtn.className = `cards-toolbar-btn${isList ? ' active' : ''}`;
+
+    gridBtn.addEventListener('click', () => {
+      block.classList.remove('list');
+      gridBtn.classList.add('active');
+      listBtn.classList.remove('active');
+    });
+
+    listBtn.addEventListener('click', () => {
+      block.classList.add('list');
+      listBtn.classList.add('active');
+      gridBtn.classList.remove('active');
+    });
+
+    toolbar.append(gridBtn, listBtn);
+    block.prepend(toolbar);
+  }
 }
